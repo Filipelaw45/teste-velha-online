3863144777ebaa8cad04603987fcdccc6b55251b
Filipe Andrade <filipe.law@gmail.com>
2023-05-08 15:59:50 -0300

    refatorando função simbolDefine

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ httpServer.listen(3000, () => {
   console.log(`Servidor iniciado na porta 3000`)
 });
 
-// codigo começa aqui
-
 //objeto com as salas ativas
 let rooms = [
   { name: 'sala1', players: ['jogador1', 'jogador2'] },
@@ -29,59 +27,18 @@ io.on("connection", (socket) => {
   console.log(`Conectado usuário ${socket.id}`)
 
   socket.on('disconnect', () => {
+    //adicionar exclusão da sala ao desconectar o player
     console.log(`Desconectado usuário ${socket.id}`)
   })
-
 
   socket.on('create-room', (playerData) => {
     createRoom(socket, playerData)
     simbolDefine(socket)
-    // console.log(io.sockets.adapter.rooms.get(data.sala))
-    // if(io.sockets.adapter.rooms.get(data.sala).size <= 2){
-    //   socket.join(data.sala)
-    //   if(io.sockets.adapter.rooms.get(data.sala).size == 1){
-    //     data.simbolo = 'X'
-    //     return
-    //   }
-    //   data.simbolo = 'O'
-    // }
-
-
-
-    // console.log(data)
-    // socket.join(data.sala)
-    // console.log(`${sala.nick} se juntou a sala ${sala.sala}`)
-    // console.log('Number of clients',io.sockets.adapter.rooms.get(sala.sala).size)
-
-
-
-    // if(io.sockets.adapter.rooms.get(sala.sala).size == 2){
-    //   socket.emit('jogador', 'O', 'Aguarde sua vez!')
-    //   io.emit('comeca')
-    //   socket.broadcast.to(sala.sala).emit('msg',`${sala.nick} entrou no jogo!`)
-    // }
-
-    // if(io.sockets.adapter.rooms.get(sala.sala).size > 2){
-    //   io.sockets.adapter.rooms.get(sala.sala).delete(socket.id)
-    //   console.log('Number of clients',io.sockets.adapter.rooms.get(sala.sala).size)
-    // }
-
-    // socket.on('passa-vez', (velha, player)=>{
-    //     io.to(sala.sala).emit('proximo', velha, player)
-    // })
-
   })
 
-
-  // socket.on('msg-de-sala', (msg, sala, nick) => {
-  //   io.to(sala).emit('msg', msg, nick)
-  // })
 });
 
-//adicionar exclusão da sala ao desconectar o player
-
 function createRoom(socket, playerData) {
-  console.log(playerData)
   socket.name = playerData.nick
   let roomExists = rooms.find(room => room.name === playerData.room)
 
@@ -101,19 +58,11 @@ function createRoom(socket, playerData) {
     rooms.push(newRoom)
     socket.join(playerData.room)
   }
-
-  console.log(rooms)
 }
 
 function simbolDefine(socket) {
   rooms.map((room) => {
-    if (room.players[0] === socket.name) {
-      socket.simbol = 'X'
-      console.log(`O jogador ${socket.name} é o ${socket.simbol}`)
-    }
-    if (room.players[1] === socket.name) {
-      socket.simbol = 'O'
-      console.log(`O jogador ${socket.name} é o ${socket.simbol}`)
-    }
+    room.players[0] === socket.name ? socket.simbol = 'X' : socket.simbol = 'O'
   })
+  console.log(`O jogador ${socket.name} é o ${socket.simbol}`)
 }

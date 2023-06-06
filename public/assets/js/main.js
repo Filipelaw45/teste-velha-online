d9b7e554f8ce0373ff7a3edac9be5637bd5e1fb4
Filipe Andrade <filipe.law@gmail.com>
2023-06-05 23:24:48 -0300

    clean code

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,53 +1,14 @@
-// let inputNick = document.getElementById('nick')
-// let btnNick = document.getElementById('btn-nick')
-let inputRoom = document.getElementById('room')
-let btnRoom = document.getElementById('btn-room')
-let symbolScreen = document.getElementById('symbol')
-let gameStatusContainer = document.getElementById('server-msg')
+const inputRoom = document.getElementById('room')
+const btnRoom = document.getElementById('btn-room')
+const symbolScreen = document.getElementById('symbol')
+const gameStatusContainer = document.getElementById('server-msg')
 
 const playerOpts = {}
 
-//
-//
-//
-//  Renomear btn-jogo
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const socket = io()
-
-// btnNick.addEventListener('click', function(e){
-//     e.preventDefault()
-//     if(inputNick.value !== ''){
-//         playerOpts.nick = inputNick.value
-//         inputNick.disabled = true
-//         btnNick.disabled = true
-//         inputRoom.focus()
-//         return
-//     }
-//     alert('Preencha o Nick!')
-// })
 
 btnRoom.addEventListener('click', function(e){
     e.preventDefault()
-    // && inputNick.value !== ''
     if(inputRoom.value !== ''){
         playerOpts.room = inputRoom.value.toLowerCase()
         btnRoom.disabled = true
@@ -55,7 +16,7 @@ btnRoom.addEventListener('click', function(e){
         createRoom()
         return
     }
-    alert('Preencha os campos!')   
+    alert('Preencha a sala!')   
 })
 
 function createRoom(){
@@ -63,11 +24,8 @@ function createRoom(){
 }
 
 function clearInput(){
-    //inputNick.value = ''
     inputRoom.value = ''
-    //inputNick.disabled = false
     inputRoom.disabled = false
-    //btnNick.disabled = false
     btnRoom.disabled = false
 }
 
@@ -76,7 +34,7 @@ function draw(gameArray, symbol){
     divVelha.innerHTML = ''
     for(let i in gameArray) {
         const button = document.createElement('button');
-        button.classList.add('btn-jogo')
+        button.classList.add('btn-game')
         button.innerText = gameArray[i]
         button.addEventListener('click',()=>{
             if(button.innerText === ''){
@@ -89,21 +47,6 @@ function draw(gameArray, symbol){
     }
 }
 
-// function disableVelha(gameArray){
-//     let btnVelha = document.querySelectorAll('.btn-jogo')
-//     for(let i in gameArray){
-//         btnVelha[i].disabled = true
-//     }
-//     draw(gameArray, null)
-// }
-
-// function activeVelha(gameArray){
-//     let btnVelha = document.querySelectorAll('.btn-jogo')
-//     for(let i in gameArray){
-//         btnVelha[i].disabled = false
-//     } 
-// }
-
 function msgGameStatus(msg){
     let p = document.createElement('p')
     p.innerText = msg
@@ -112,36 +55,22 @@ function msgGameStatus(msg){
 }
 
 socket.on('symbol', symbol=>{
-
     symbolScreen.innerText = ''
     symbolScreen.innerText = `Você é: ${symbol}`
-
     playerOpts.symbol = symbol
 })
 
 socket.on('game-progress', velha => draw(velha, playerOpts.symbol))
 
-socket.on('server-msg', (msg)=>{
-    msgGameStatus(msg)
-})
-
-// socket.on('ativa', ()=>{
-//     activeVelha()
-// })
+socket.on('server-msg', (msg)=> msgGameStatus(msg) )
 
 socket.on('room-erro', (msg)=>{
     alert(msg)
     clearInput()
 })
 
-// socket.on('disable-game', (velha)=>{
-//     console.log('entrei aqui no disable game')
-//     disableVelha(velha)
-// })
-
 socket.on('resetRoom', ()=>{
     setInterval(()=>{
         location.reload()
     }, 2500)
-    //location.reload()
 })
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 })
 
 httpServer.listen(3000, () => {
-  console.log(`Servidor iniciado na porta 3000`)
+  console.log(`Servidor iniciado em localhost:3000`)
 })
 
 //objeto com as salas ativas
@@ -30,13 +30,9 @@ let winSequence = [
   [2, 4, 6]
 ]
 
-
 io.on("connection", (socket) => {
 
-  console.log(`Conectado usuário ${socket.id}`)
-
   socket.on('disconnect', () => {
-    console.log(`Desconectado usuário ${socket.id}`)
 
     if (rooms != []) {
       rooms.find((room, index) => {
@@ -56,10 +52,8 @@ io.on("connection", (socket) => {
           rooms.splice(index, 1)
           return
         }
-
       })
     }
-
   })
 
   socket.on('create-room', (playerData) => {
@@ -67,73 +61,32 @@ io.on("connection", (socket) => {
     symbolDefine(playerData)
     inicializeGame(playerData)
 
-
     socket.on('move', (positionMove, symbol) => {
-      let roomExists = rooms.find(room => room.room === playerData.room)
-      let { room, gameStatus } = roomExists
-      if (gameStatus.turn == symbol) {
-        gameStatus.velha[positionMove] = symbol
-        gameStatus.turn == 'X' ? gameStatus.turn = 'O' : gameStatus.turn = 'X'
-        checkGame(playerData)
-      } else {
-        io.to(socket.id).emit('room-erro', 'Não é a sua vez!')
-      }
-      io.to(room).emit('game-progress', gameStatus.velha)
-
-      if(!gameStatus.gameOver){
-        io.to(room).emit('server-msg', `Vez do: ${gameStatus.turn}`)
-      } 
+      makeMove(socket, playerData, symbol, positionMove) 
     })
-
   })
-
-});
+})
 
 function createRoom(socket, playerData) {
-  //playerData.nick
-  let roomExists = rooms.find(room => room.room === playerData.room)
+  let currentRoom = findRoom(playerData)
 
-  if (roomExists) {
-    if (roomExists.players.id.length < 2) {
-      socket.join(`${playerData.room}`)
-      roomExists.players.id.push(socket.id)
-      //roomExists.players.name.push(playerData.nick)
-    } else {
-      socket.emit('room-erro', 'A sala está cheia!')
-      return
-    }
+  if (currentRoom) {
+    joinGame(playerData, currentRoom, socket)
   } else {
-    const newRoom = {
-      room: playerData.room,
-      players: {
-        player1: 'X',
-        player2: 'O',
-        id: [socket.id],
-       // name: [playerData.nick],
-      },
-      gameStatus: {
-        velha: ['', '', '', '', '', '', '', '', ''],
-        gameOver: false,
-        turn: 'X'
-      }
-    }
-    rooms.push(newRoom)
-    socket.join(playerData.room)
+    createNewRoom(socket, playerData)
   }
 }
 
 function symbolDefine(playerData) {
-  let socketRoom = rooms.find(room => room.room === playerData.room)
-  let { players } = socketRoom
-
+  let currentRoom = findRoom(playerData)
+  let { players } = currentRoom
   io.to(players.id[0]).emit('symbol', players.player1)
   io.to(players.id[1]).emit('symbol', players.player2)
 }
 
 function inicializeGame(playerData) {
-  let socketRoom = rooms.find(room => room.room === playerData.room)
-  let { room, gameStatus, players } = socketRoom
-  console.log(socketRoom)
+  let { room, gameStatus, players } = findRoom(playerData)
+
   if (players.id.length == 2) {
     io.to(room).emit('server-msg', `Vez do: ${gameStatus.turn}`)
     io.to(room).emit('game-progress', gameStatus.velha)
@@ -144,44 +97,95 @@ function inicializeGame(playerData) {
 
 
 function checkGame(playerData) {
-  let socketRoom = rooms.find(room => room.room === playerData.room)
-  let { room, gameStatus} = socketRoom
+  let {room, gameStatus} = findRoom(playerData)
 
   for (let i in winSequence) {
     if (gameStatus.velha[winSequence[i][0]] === 'X' &&
       gameStatus.velha[winSequence[i][1]] === 'X' &&
       gameStatus.velha[winSequence[i][2]] === 'X') {
-      // io.to(room).emit('room-erro', 'jogador X ganhou!')
-      gameStatus.gameOver = true
-      io.to(room).emit('room-erro', 'Jogador X ganhou!')
-      io.to(room).emit('resetRoom')
-      io.to(room).emit('disable-game', gameStatus.velha)
-      io.to(room).emit('server-msg', 'Jogador X ganhou!')
+        winMsg(room, gameStatus, 'X')
       break
     }
   }
 
   for (let i in winSequence) {
+
     if (gameStatus.velha[winSequence[i][0]] === 'O' &&
-      gameStatus.velha[winSequence[i][1]] === 'O' &&
-      gameStatus.velha[winSequence[i][2]] === 'O') {
-      // io.to(room).emit('room-erro', 'jogador O ganhou!')
-      gameStatus.gameOver = true
-      io.to(room).emit('room-erro', 'Jogador O ganhou!')
-      io.to(room).emit('resetRoom')
-      io.to(room).emit('disable-game', gameStatus.velha)
-      io.to(room).emit('server-msg', 'Jogador O ganhou!')
+    gameStatus.velha[winSequence[i][1]] === 'O' &&
+    gameStatus.velha[winSequence[i][2]] === 'O') {
+      winMsg(room, gameStatus, 'O')
       break
     }
   }
 
   if (!gameStatus.velha.includes('') && !gameStatus.gameOver) {
-    gameStatus.gameOver = true
-    io.to(room).emit('room-erro', 'Empate')
-    io.to(room).emit('resetRoom')
-    io.to(room).emit('disable-game', gameStatus.velha)
-    io.to(room).emit('server-msg', 'Empate!')
+    winMsg(room, gameStatus, undefined)
+  }
+}
+
+function findRoom(roomData){
+  return rooms.find(room => room.room === roomData.room)
+}
+
+function joinGame(playerData, room, socket){
+  if (room.players.id.length < 2) {
+    socket.join(`${playerData.room}`)
+    room.players.id.push(socket.id)
+  } else {
+    socket.emit('room-erro', 'A sala está cheia!')
+  }
+}
+
+function createNewRoom(socket, playerData){
+  const newRoom = {
+    room: playerData.room,
+    players: {
+      player1: 'X',
+      player2: 'O',
+      id: [socket.id],
+    },
+    gameStatus: {
+      velha: ['', '', '', '', '', '', '', '', ''],
+      gameOver: false,
+      turn: 'X'
+    }
+  }
+  rooms.push(newRoom)
+  socket.join(playerData.room)
+}
+
+function makeMove(socket, playerData, symbol, positionMove){
+  let currentRoom = findRoom(playerData)
+  let { room, gameStatus } = currentRoom
+
+  if (gameStatus.turn == symbol) {
+    gameStatus.velha[positionMove] = symbol
+    gameStatus.turn == 'X' ? gameStatus.turn = 'O' : gameStatus.turn = 'X'
+    checkGame(playerData)
+  } else {
+    io.to(socket.id).emit('room-erro', 'Não é a sua vez!')
   }
 
+  io.to(room).emit('game-progress', gameStatus.velha)
+
+  if(!gameStatus.gameOver){
+    io.to(room).emit('server-msg', `Vez do: ${gameStatus.turn}`)
+  } 
+
+}
+
+function winMsg(room, statusGame, winner){
+  statusGame.gameOver = true
+
+  if(winner){
+    io.to(room).emit('room-erro', `Jogador ${winner} ganhou!`)
+    io.to(room).emit('server-msg', `Jogador ${winner} ganhou!`)
+  }else{
+    io.to(room).emit('room-erro', `Empate!`)
+    io.to(room).emit('server-msg', `Empate!`)
+  }
+
+  io.to(room).emit('resetRoom')
+  io.to(room).emit('disable-game', statusGame.velha)
 
 }
